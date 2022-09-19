6b900e8add68cc2e66f7652ce32eab49eaf45d09
Swathi M <57789699+SwathiMaran97@users.noreply.github.com>
2022-09-19 16:31:15 +0530

    Update and rename try.py to SeleniumTestcase.py

--- SeleniumTestcase.py
+++ SeleniumTestcase.py
@@ -2,13 +2,13 @@ import sys
 import os
 from selenium import webdriver
 
-# path = "https://vconnect.inesssolutions.net/login"
+# path = "https://beautyproducts/login"
 browser= webdriver.Chrome(executable_path='C:/path/to/chromedriver.exe')
-browser.get("https://vconnect.inesssolutions.net/login")
+browser.get("https://beautyproducts/login")
 
 def create():
     python_button = browser.find_elements_by_xpath("//*[@id=orangeForm-email]")[0]
-    python_button.send_keys('swathi@inesssolutions.com')
+    python_button.send_keys('swathi@gmail.com')
     # python_button = browser.find_elements_by_xpath("//*[@id="orangeForm-pass"]")[0]
     # python_button.send_keys('')
 if __name__ == '__main__':
